fde987afbb886035e83e78f52158260125b01cd2
The0x539 <the0x539@gmail.com>
2021-12-02 13:46:20 -0600

    clean up color module

--- src/color.rs
+++ src/color.rs
@@ -7,49 +7,20 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn new(red: f32, green: f32, blue: f32, opacity: f32) -> Color {
-        Color {
-            red: if red > 1.0 {
-                1.0
-            } else if red < 0.0 {
-                0.0
-            } else {
-                red
-            },
-            green: if green > 1.0 {
-                1.0
-            } else if green < 0.0 {
-                0.0
-            } else {
-                green
-            },
-            blue: if blue > 1.0 {
-                1.0
-            } else if blue < 0.0 {
-                0.0
-            } else {
-                blue
-            },
-            opacity: if opacity > 1.0 {
-                1.0
-            } else if opacity < 0.0 {
-                0.0
-            } else {
-                opacity
-            },
+    pub fn new(red: f32, green: f32, blue: f32, opacity: f32) -> Self {
+        let [red, green, blue, opacity] = [red, green, blue, opacity].map(|x| x.clamp(0.0, 1.0));
+        Self {
+            red,
+            green,
+            blue,
+            opacity,
         }
     }
 
-    pub fn blend(&self, other: &Color, ratio: f32) -> Color {
-        let ratio = if ratio > 1.0 {
-            1.0
-        } else if ratio < 0.0 {
-            0.0
-        } else {
-            ratio
-        };
+    pub fn blend(&self, other: &Color, ratio: f32) -> Self {
+        let ratio = ratio.clamp(0.0, 1.0);
 
-        Color {
+        Self {
             red: self.red + ((other.red - self.red) * ratio),
             green: self.green + ((other.green - self.green) * ratio),
             blue: self.blue + ((other.blue - self.blue) * ratio),
@@ -58,9 +29,7 @@ impl Color {
     }
 
     pub fn as_argb8888(&self) -> u32 {
-        ((255.0 * self.opacity) as u32 & 0xFF) << 24
-            | ((255.0 * self.red) as u32 & 0xFF) << 16
-            | ((255.0 * self.green) as u32 & 0xFF) << 8
-            | ((255.0 * self.blue) as u32 & 0xFF)
+        let argb = [self.opacity, self.red, self.green, self.blue];
+        u32::from_be_bytes(argb.map(|x| (x * 255.0) as u8))
     }
 }
