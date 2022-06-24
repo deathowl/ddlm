c11c7304b2c1d3b69f6c18e098b3ef87bae768f8
Wesley Moore <wes@wezm.net>
2022-06-24 17:43:18 +1000

    Update dependencies

--- src/draw.rs
+++ src/draw.rs
@@ -12,10 +12,10 @@ pub static ROBOTO_REGULAR_FONT_DATA: &[u8] = include_bytes!("../fonts/Roboto-Reg
 
 lazy_static! {
     pub static ref DEJAVUSANS_MONO: RustFont<'static> =
-        RustFont::from_bytes(DEJAVUSANS_MONO_FONT_DATA as &[u8])
+        RustFont::try_from_bytes(DEJAVUSANS_MONO_FONT_DATA as &[u8])
             .expect("error constructing DejaVuSansMono");
     pub static ref ROBOTO_REGULAR: RustFont<'static> =
-        RustFont::from_bytes(ROBOTO_REGULAR_FONT_DATA as &[u8])
+        RustFont::try_from_bytes(ROBOTO_REGULAR_FONT_DATA as &[u8])
             .expect("error constructing Roboto-Regular");
 }
 
